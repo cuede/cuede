c076c6bca68f9fa28435e467c033978fcff30dc6
smnicolas <smnicolas@gmail.com>
2018-04-27 15:49:54 -0300

    Hago unos Models

--- secomplican/enunciados/models.py
+++ secomplican/enunciados/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Practica(models.Model):
+    enunciados = models.ForeygnKey(Enunciado)
+    materia = models.ForeignKey(Materia)
+    # enunciados
+    
+class Enunciado(models.Model):
+    practicas = models.ManyToManyField(Practica)
+    texto = models.CharField()
+
+
+class Materia(models.Model):
+    nombre = models.CharField(max_length=1023)
+    # practicas
