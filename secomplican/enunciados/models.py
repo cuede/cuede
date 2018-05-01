4f30b5858f73e3517630bed8cfdea90f6998a8d7
Eric Brandwein <brandweineric@gmail.com>
2018-05-01 19:11:41 -0300

    Arregla models.py y agrega modelos de Etapa y Solucion

--- secomplican/enunciados/models.py
+++ secomplican/enunciados/models.py
@@ -1,15 +1,32 @@
 from django.db import models
 
-class Practica(models.Model):
-    enunciados = models.ForeygnKey(Enunciado)
-    materia = models.ForeignKey(Materia)
-    # enunciados
-    
-class Enunciado(models.Model):
-    practicas = models.ManyToManyField(Practica)
-    texto = models.CharField()
-
 
 class Materia(models.Model):
     nombre = models.CharField(max_length=1023)
-    # practicas
+
+
+# No estoy seguro de que Etapa haga falta, podemos simplemente poner los datos de la etapa
+# en cada practica o parcial.
+class Etapa(models.Model):
+    """
+    Representa la etapa de un año: 1er cuatrimestre, 2do cuatrimestre, o verano.
+    """
+    anio = models.IntegerField()
+    # 1 = 1er cuatrimestre, 2 = 2do cuatrimestre, 3 = verano
+    # Quiza haya una forma mas linda de representar en que cuatri estamos.
+    cuatrimestre = models.IntegerField()
+
+
+class Practica(models.Model):
+    materia = models.ForeignKey(Materia, models.CASCADE)
+    etapa = models.ForeignKey(Etapa, models.CASCADE)
+
+
+class Enunciado(models.Model):
+    practica = models.ForeignKey(Practica, models.CASCADE)
+    texto = models.TextField()
+
+
+class Solucion(models.Model):
+    enunciado = models.ForeignKey(Enunciado, models.CASCADE)
+    texto = models.TextField()
