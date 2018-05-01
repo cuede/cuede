1f97d79f3a71b510a24f338ddb550fe0db0c3b34
Eric Brandwein <brandweineric@gmail.com>
2018-05-01 19:15:13 -0300

    Arregla views.py para que siga al PEP8

--- secomplican/enunciados/views.py
+++ secomplican/enunciados/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("Se complican :)")
 
-def materias(resuest):    
-    return HttpResponse("Ac'a va el listado de las materias.");
+
+def materias(request):
+    return HttpResponse("Acá va el listado de las materias.")
