c508021aff5047ff9fbe47c0b5db629bfe214a4a
Eric Brandwein <brandweineric@gmail.com>
2018-05-03 00:42:26 -0300

    Elimina tiene_practica_con_materia

--- secomplican/enunciados/models_utils.py
+++
@@ -1,7 +0,0 @@
-def tiene_practica_con_materia(cuatrimestre, materia):
-    """
-    Define si el Cuatrimestre tiene alguna práctica que pretenezca a la materia.
-
-    :param materia: Nombre de la materia.
-    """
-    return any(practica.materia.nombre == materia for practica in cuatrimestre.practica_set.all())
