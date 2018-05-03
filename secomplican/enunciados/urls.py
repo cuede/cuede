29151ecb2418871814c116653fc54a3a556e8ae8
Eric Brandwein <brandweineric@gmail.com>
2018-05-03 01:19:16 -0300

    Agrega view de practica

--- secomplican/enunciados/urls.py
+++ secomplican/enunciados/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('materias', views.MateriasView.as_view(), name='materias'),
     path('<nombre>', views.materia, name='materia'),
-    path('<materia>/<cuatrimestre>/practica<int:practica>/<int:numero>', views.enunciado, name='enunciado')
+    path('<materia>/<int:anio>/<cuatrimestre>/practica<int:numero>', views.practica, name='practica'),
+    path('<materia>/<int:anio>/<cuatrimestre>/practica<int:practica>/<int:numero>', views.enunciado, name='enunciado'),
 ]
--- secomplican/enunciados/views.py
+++ secomplican/enunciados/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.views import generic
 from django.shortcuts import get_object_or_404, get_list_or_404, render
-from enunciados.models import Materia, Enunciado
+from enunciados.models import Materia, Cuatrimestre, Practica, Enunciado
 
 
 def index(request):
@@ -17,6 +17,33 @@ def materia(request, nombre):
     return render(request, 'enunciados/materia.html', {'materia': objeto})
 
 
-def enunciado(request, materia, cuatrimestre, practica, numero):
+def __parsear_cuatrimestre(cuatrimestre):
+    """
+    Devuelve el numero de cuatrimestre parseando el parámetro de una url.
+
+    :returns: None si es que cuatrimestre no es uno entre '1cuatri', '2cuatri' o 'verano'
+    """
+    if cuatrimestre == '1cuatri':
+        return Cuatrimestre.PRIMERO
+    elif cuatrimestre == '2cuatri':
+        return Cuatrimestre.SEGUNDO
+    elif cuatrimestre == 'verano':
+        return Cuatrimestre.VERANO
+    else:
+        return None
+
+
+def practica(request, materia, anio, cuatrimestre, numero):
+    numero_cuatri = __parsear_cuatrimestre(cuatrimestre)
+    if not numero_cuatri:
+        return HttpResponseBadRequest('El cuatrimestre puede ser uno entre: "1cuatri", "2cuatri", "verano"')
+
+    practica = get_object_or_404(Practica, materia__nombre=materia, cuatrimestre__anio=anio,
+                                 cuatrimestre__cuatrimestre=numero_cuatri,
+                                 numero=numero)
+    return render(request, 'enunciados/practica.html', {'practica': practica})
+
+
+def enunciado(request, materia, anio, cuatrimestre, practica, numero):
     encontrado = get_list_or_404(Enunciado, practica__materia__nombre=materia)[0]
     return HttpResponse('{}'.format(encontrado))
