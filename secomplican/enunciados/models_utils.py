8ec2d9729e28759dafd143f300c3755d10d4936a
Eric Brandwein <brandweineric@gmail.com>
2018-05-04 20:26:57 -0300

    Crea parciales_de_materia_ordenados()

--- secomplican/enunciados/models_utils.py
+++ secomplican/enunciados/models_utils.py
@@ -1,20 +1,100 @@
-def parciales_ordenados(materia):
-    """Devuelve todos los parciales de la materia seleccionada ordenados de ultimo cuatrimestre a primero."""
-    return materia.conjuntodeenunciados_set \
-        .filter(parcial__isnull=False) \
-        .order_by('-cuatrimestre__anio', '-cuatrimestre__cuatrimestre')
+from enunciados.models import Cuatrimestre
+
+
+def __castear_a_parciales(conjuntos_de_enunciados):
+    return [conjunto.parcial for conjunto in conjuntos_de_enunciados]
+
+
+def __separar_por_numero(parciales):
+    """Devuelve un conjunto de conjuntos de parciales, separados por numero."""
+    separados = {}
+    for parcial in parciales:
+        numero = parcial.numero
+        if numero in separados:
+            separados[numero].append(parcial)
+        else:
+            separados[numero] = [parcial]
+
+    return separados
+
+
+def __comparador_numero_cuatrimestre(numero_cuatrimestre):
+    """
+    Devuelve un número que se puede usar para comparar entre
+    números de cuatrimestre de anterior a posterior.
+    """
+    if numero_cuatrimestre == Cuatrimestre.VERANO:
+        return 1
+    elif numero_cuatrimestre == Cuatrimestre.PRIMERO:
+        return 2
+    else:
+        return 3
+
+
+def __comparador_cuatrimestre(cuatrimestre):
+    """
+    Devuelve un número que se puede usar para comparar entre cuatrimestres
+    para determinar cuál es más reciente.
+    """
+    comparador_numero = __comparador_numero_cuatrimestre(cuatrimestre.numero)
+    string_comparador = '{anio}{numero}' \
+        .format(anio=cuatrimestre.anio, numero=comparador_numero)
+    return int(string_comparador)
+
+
+def __comparador_parcial(parcial):
+    """
+    Devuelve un número que se puede usar para comparar entre parciales.
+    """
+    comparador_recuperatorio = 1 if parcial.recuperatorio else 0
+    string_comparador = '{}{}'.format(__comparador_cuatrimestre(parcial.cuatrimestre), comparador_recuperatorio)
+    return int(string_comparador)
+
+
+def __ordenar_por_cuatrimestre(parciales):
+    """
+    Ordena los parciales de más reciente a menos reciente.
+
+    Pone a los recuperatorios antes que los parciales.
+    """
+    return sorted(parciales, key=__comparador_parcial, reverse=True)
+
+
+def parciales_de_materia(materia):
+    """
+    Devuelve una lista de los parciales que pertenecen a la materia.
+
+    :param materia: No puede ser None.
+    """
+    if not materia:
+        raise ValueError('Materia no debería ser None.')
+
+    conjuntos = materia.conjuntodeenunciados_set.filter(parcial__isnull=False)
+    return __castear_a_parciales(conjuntos)
+
+
+def parciales_de_materia_ordenados(materia):
+    """
+    Devuelve una lista de conjuntos de parciales,
+    cada conjunto ordenado por cuatrimestre.
+
+    :param materia: No puede ser None.
+    """
+    parciales = parciales_de_materia(materia)
+    parciales_por_numero = __separar_por_numero(parciales)
+    for numero, parciales_de_numero in parciales_por_numero.items():
+        parciales_por_numero[numero] = __ordenar_por_cuatrimestre(parciales_de_numero)
+    return parciales_por_numero
 
 
 def ultimas_practicas_ordenadas(materia):
     """
     Devuelve todas las practicas de la materia que esten en el ultimo cuatrimestre en el que
     hay practicas para la materia.
-    :param materia:
-    :return:
     """
     practicas_descendientes = materia.conjuntodeenunciados_set \
         .filter(practica__isnull=False) \
-        .order_by('-cuatrimestre__anio', '-cuatrimestre__cuatrimestre')
+        .order_by('-cuatrimestre__anio', '-cuatrimestre__numero')
     if practicas_descendientes:
         ultimo_cuatrimestre = practicas_descendientes[0].cuatrimestre
         practicas_descendientes = practicas_descendientes.filter(cuatrimestre=ultimo_cuatrimestre)
---
+++ secomplican/enunciados/tests/tests_models_utils.py
@@ -0,0 +1,147 @@
+from django.test import TestCase
+from enunciados.models import Materia, Cuatrimestre, Parcial
+from enunciados.models_utils import *
+
+
+class ModelsUtilsTests(TestCase):
+    def setUp(self):
+        self.materia = Materia(nombre='materia')
+        self.materia.save()
+
+    def test_parciales_de_materia_ordenados_sin_materia(self):
+        """Debería levantar un ValueError."""
+        with self.assertRaises(ValueError):
+            parciales_de_materia_ordenados(None)
+
+    def test_parciales_de_materia_ordenados_sin_parciales(self):
+        """Debería devolver un diccionario vacía."""
+        self.assertEquals(len(parciales_de_materia_ordenados(self.materia)), 0)
+
+    def test_parciales_de_materia_ordenados_con_primeros_parciales_con_igual_anio(self):
+        """
+        Debería devolver un diccionario con los primeros parciales ordenados
+        en la primer posición.
+        """
+        cuatri1 = Cuatrimestre(anio=2018, numero=Cuatrimestre.VERANO)
+        cuatri1.save()
+        cuatri2 = Cuatrimestre(anio=2018, numero=Cuatrimestre.PRIMERO)
+        cuatri2.save()
+        cuatri3 = Cuatrimestre(anio=2018, numero=Cuatrimestre.SEGUNDO)
+        cuatri3.save()
+
+        parcial1 = Parcial(materia=self.materia, cuatrimestre=cuatri1, numero=1)
+        parcial2 = Parcial(materia=self.materia, cuatrimestre=cuatri2, numero=1)
+        parcial3 = Parcial(materia=self.materia, cuatrimestre=cuatri3, numero=1)
+
+        # Los guardamos en desorden
+        parcial2.save()
+        parcial1.save()
+        parcial3.save()
+
+        # Los ordenamos de último a primero
+        parciales = [parcial3, parcial2, parcial1]
+        ordenados = parciales_de_materia_ordenados(self.materia)
+        self.assertEquals(len(ordenados), 1)
+        primeros_parciales = ordenados[1]
+        self.assertEquals(len(primeros_parciales), len(parciales))
+        for index, parcial in enumerate(primeros_parciales):
+            self.assertEquals(parcial, parciales[index])
+
+    def test_parciales_de_materia_ordenados_con_primeros_parciales_diferente_anio(self):
+        """
+        Debería devolver el diccionario con los primeros parciales ordenados
+        de último año a primero.
+        """
+        cuatri1 = Cuatrimestre(anio=2018, numero=Cuatrimestre.PRIMERO)
+        cuatri1.save()
+        cuatri2 = Cuatrimestre(anio=2018, numero=Cuatrimestre.VERANO)
+        cuatri2.save()
+        cuatri3 = Cuatrimestre(anio=2017, numero=Cuatrimestre.SEGUNDO)
+        cuatri3.save()
+        cuatri4 = Cuatrimestre(anio=2017, numero=Cuatrimestre.PRIMERO)
+        cuatri4.save()
+
+        parcial1 = Parcial(materia=self.materia, cuatrimestre=cuatri1, numero=1)
+        parcial2 = Parcial(materia=self.materia, cuatrimestre=cuatri2, numero=1)
+        parcial3 = Parcial(materia=self.materia, cuatrimestre=cuatri3, numero=1)
+        parcial4 = Parcial(materia=self.materia, cuatrimestre=cuatri4, numero=1)
+
+        # Los guardamos en desorden
+        parcial2.save()
+        parcial1.save()
+        parcial4.save()
+        parcial3.save()
+
+        parciales = [parcial1, parcial2, parcial3, parcial4]
+        ordenados = parciales_de_materia_ordenados(self.materia)
+        self.assertEquals(len(ordenados), 1)
+        primeros_parciales = ordenados[1]
+        self.assertEquals(len(primeros_parciales), len(parciales))
+        for index, parcial in enumerate(primeros_parciales):
+            self.assertEquals(parcial, parciales[index])
+
+    def test_parciales_de_materia_ordenados_con_parciales_de_numeros_diferentes(self):
+        """
+        Debería devolver un diccionario con posiciones igual a la cantidad de
+        números diferentes.
+        """
+        cuatri1 = Cuatrimestre(anio=2018, numero=Cuatrimestre.PRIMERO)
+        cuatri1.save()
+        cuatri2 = Cuatrimestre(anio=2018, numero=Cuatrimestre.VERANO)
+        cuatri2.save()
+        cuatri3 = Cuatrimestre(anio=2017, numero=Cuatrimestre.SEGUNDO)
+        cuatri3.save()
+        cuatri4 = Cuatrimestre(anio=2017, numero=Cuatrimestre.PRIMERO)
+        cuatri4.save()
+
+        primeros = [
+            Parcial(materia=self.materia, cuatrimestre=cuatri1, numero=1),
+            Parcial(materia=self.materia, cuatrimestre=cuatri2, numero=1),
+        ]
+
+        segundos = [
+            Parcial(materia=self.materia, cuatrimestre=cuatri1, numero=2),
+            Parcial(materia=self.materia, cuatrimestre=cuatri3, numero=2),
+            Parcial(materia=self.materia, cuatrimestre=cuatri4, numero=2),
+        ]
+
+        for parcial in primeros:
+            parcial.save()
+
+        for parcial in segundos:
+            parcial.save()
+
+        ordenados = parciales_de_materia_ordenados(self.materia)
+        self.assertEquals(len(ordenados), 2)
+        primeros_parciales = ordenados[1]
+        self.assertEquals(len(primeros_parciales), len(primeros))
+        for index, parcial in enumerate(primeros_parciales):
+            self.assertEquals(parcial, primeros[index])
+        segundos_parciales = ordenados[2]
+        self.assertEquals(len(segundos_parciales), len(segundos))
+        for index, parcial in enumerate(segundos_parciales):
+            self.assertEquals(parcial, segundos[index])
+
+    def test_parciales_de_materia_ordenados_con_recuperatorios(self):
+        """Debería devolver los recuperatorios antes que los parciales."""
+        cuatri1 = Cuatrimestre(anio=2018, numero=Cuatrimestre.PRIMERO)
+        cuatri1.save()
+        cuatri2 = Cuatrimestre(anio=2018, numero=Cuatrimestre.SEGUNDO)
+        cuatri2.save()
+
+        parcial1 = Parcial(materia=self.materia, cuatrimestre=cuatri1, numero=1)
+        recu = Parcial(
+            materia=self.materia, cuatrimestre=cuatri1, numero=1, recuperatorio=True)
+        parcial2 = Parcial(materia=self.materia, cuatrimestre=cuatri2, numero=1)
+
+        parcial1.save()
+        parcial2.save()
+        recu.save()
+
+        parciales = [parcial2, recu, parcial1]
+        ordenados = parciales_de_materia_ordenados(self.materia)
+        self.assertEquals(len(ordenados), 1)
+        primeros_parciales = ordenados[1]
+        self.assertEquals(len(primeros_parciales), len(parciales))
+        for index, parcial1 in enumerate(primeros_parciales):
+            self.assertEquals(parcial1, parciales[index])
