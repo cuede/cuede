fdfd364367834daa6eecfe9146e8d50dc14cba49
Eric Brandwein <brandweineric@gmail.com>
2018-06-01 17:06:17 -0300

    Agrega los models al admin.py

--- secomplican/enunciados/admin.py
+++ secomplican/enunciados/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import *
 # Register your models here.
+
+admin.site.register(Materia)
+admin.site.register(Cuatrimestre)
+admin.site.register(Practica)
+admin.site.register(Parcial)
+admin.site.register(Final)
+admin.site.register(Enunciado)
+admin.site.register(VersionTextoEnunciado)
+admin.site.register(VersionTextoSolucion)
