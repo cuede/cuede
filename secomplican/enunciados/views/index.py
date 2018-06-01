fe0d5200ce132003de078052592cc8cd3d3335ee
Eric Brandwein <brandweineric@gmail.com>
2018-06-01 17:02:31 -0300

    Pone el TIME_ZONE en Buenos Aires

--- secomplican/enunciados/views/index.py
+++ secomplican/enunciados/views/index.py
@@ -4,7 +4,7 @@ from django.views import generic
 
 from enunciados import cuatrimestres_url_parser
 from enunciados import models_utils
-from enunciados.models import Materia, Practica, Parcial, Final, Enunciado
+from enunciados.models import Materia, Practica, Parcial, Final
 
 
 def index(request):
@@ -60,39 +60,3 @@ def final(request, materia, anio, mes, dia):
     return render_conjunto_de_enunciados(request, finales)
 
 
-def render_enunciado(enunciado_elegido):
-    return HttpResponse('{}'.format(enunciado_elegido.versiones.ultima()))
-
-
-def enunciado(request, materia, anio, cuatrimestre, conjunto_de_enunciados, tipo_conjunto, numero):
-    encontrados = Enunciado.objects.filter(
-        conjunto__materia__nombre=materia,
-        conjunto__cuatrimestre__anio=anio,
-        conjunto__cuatrimestre__numero=cuatrimestres_url_parser.url_a_numero(cuatrimestre),
-        numero=numero,
-    )
-    if tipo_conjunto == 'practica':
-        encontrados = encontrados.filter(conjunto__practica__isnull=False) \
-            .filter(conjunto__practica__numero=conjunto_de_enunciados)
-    else:
-        encontrados = encontrados.filter(conjunto__parcial__isnull=False)
-        es_recuperatorio = tipo_conjunto == 'recuperatorio'
-        encontrados = encontrados.filter(conjunto__parcial__recuperatorio=es_recuperatorio) \
-            .filter(conjunto__parcial__numero=conjunto_de_enunciados)
-
-    enunciado_encontrado = encontrados[0]
-    return render_enunciado(enunciado_encontrado)
-
-
-def enunciado_final(request, materia, anio, mes, dia, numero):
-    encontrado = get_object_or_404(
-        Enunciado,
-        conjunto__materia__nombre=materia,
-        numero=numero,
-        conjunto__final__isnull=False,
-        conjunto__final__fecha__year=anio,
-        conjunto__final__fecha__month=mes,
-        conjunto__final__fecha__day=dia,
-    )
-
-    return render_enunciado(encontrado)
--- secomplican/secomplican/settings.py
+++ secomplican/secomplican/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
 USE_I18N = True
 
