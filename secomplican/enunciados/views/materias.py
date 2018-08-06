07e8e402e2e440e5e139619c71f0c334434f95af
Eric Brandwein <brandweineric@gmail.com>
2018-08-06 17:16:45 -0300

    Usa practica.get_absolute_url en materia.html

--- secomplican/enunciados/views/materias.py
+++ secomplican/enunciados/views/materias.py
@@ -14,11 +14,6 @@ def materia(request, nombre):
         'materia': get_object_or_404(Materia, nombre=nombre),
     }
     contexto['practicas'] = models_utils.ultimas_practicas_ordenadas(contexto['materia'])
-    if contexto['practicas']:
-        ultimo_cuatrimestre = contexto['practicas'][0].cuatrimestre
-        contexto['url_cuatrimestre_practicas'] = cuatrimestres_url_parser.numero_a_url(ultimo_cuatrimestre.numero)
-        contexto['ultimo_anio_practicas'] = ultimo_cuatrimestre.anio
-
     contexto['parciales'] = models_utils.parciales_de_materia_ordenados(contexto['materia'])
     contexto['finales'] = models_utils.finales_de_materia_ordenados(contexto['materia'])
     return render(request, 'enunciados/materia.html', contexto)
