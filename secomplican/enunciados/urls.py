cdc4835d57223eab927309c9629595242516a889
Eric Brandwein <brandweineric@gmail.com>
2018-08-07 01:22:01 -0300

    Agrega / al final de todas las urls
    django por default, si la url pedida no termina
    en /, le agrega un / al final y se fija desde ahi

--- secomplican/enunciados/urls.py
+++ secomplican/enunciados/urls.py
@@ -7,66 +7,66 @@ from enunciados.views.enunciados import soluciones
 
 urlpatterns = [
     path('', index.index, name='index'),
-    path('materias', materias.MateriasView.as_view(), name='materias'),
-    path('<nombre>', materias.materia, name='materia'),
-    path('<materia>/<int:anio>/<cuatrimestre>/practicas/<int:numero>',
+    path('materias/', materias.MateriasView.as_view(), name='materias'),
+    path('<nombre>/', materias.materia, name='materia'),
+    path('<materia>/<int:anio>/<cuatrimestre>/practicas/<int:numero>/',
          conjuntos_de_enunciados.practica, name='practica'),
-    path('<materia>/<int:anio>/<cuatrimestre>/parciales/<int:numero>', conjuntos_de_enunciados.parcial, name='parcial'),
-    path('<materia>/<int:anio>/<cuatrimestre>/recuperatorios/<int:numero>',
+    path('<materia>/<int:anio>/<cuatrimestre>/parciales/<int:numero>/', conjuntos_de_enunciados.parcial, name='parcial'),
+    path('<materia>/<int:anio>/<cuatrimestre>/recuperatorios/<int:numero>/',
          conjuntos_de_enunciados.parcial, {'recuperatorio': True},
          name='recuperatorio'),
-    path('<materia>/finales/<int:anio>/<int:mes>/<int:dia>', conjuntos_de_enunciados.final, name='final'),
+    path('<materia>/finales/<int:anio>/<int:mes>/<int:dia>/', conjuntos_de_enunciados.final, name='final'),
 
     # Enunciados
     path(
-        '<materia>/<int:anio>/<cuatrimestre>/practicas/<int:numero_practica>/<int:numero>',
+        '<materia>/<int:anio>/<cuatrimestre>/practicas/<int:numero_practica>/<int:numero>/',
         enunciados.enunciado_practica,
         name='enunciado_practica'
     ),
     path(
-        '<materia>/<int:anio>/<cuatrimestre>/parciales/<int:numero_parcial>/<int:numero>',
+        '<materia>/<int:anio>/<cuatrimestre>/parciales/<int:numero_parcial>/<int:numero>/',
         enunciados.enunciado_parcial,
         {'es_recuperatorio': False},
         name='enunciado_parcial'
     ),
     path(
-        '<materia>/<int:anio>/<cuatrimestre>/recuperatorios/<int:numero_parcial>/<int:numero>',
+        '<materia>/<int:anio>/<cuatrimestre>/recuperatorios/<int:numero_parcial>/<int:numero>/',
         enunciados.enunciado_parcial,
         {'es_recuperatorio': True},
         name='enunciado_recuperatorio'
     ),
     path(
-        '<materia>/finales/<int:anio>/<int:mes>/<int:dia>/<int:numero>',
+        '<materia>/finales/<int:anio>/<int:mes>/<int:dia>/<int:numero>/',
         enunciados.enunciado_final,
         name='enunciado_final'
     ),
 
     # Soluciones
     path(
-        '<materia>/<int:anio>/<cuatrimestre>/practicas/<int:numero_practica>/<int:numero>/nuevaSolucion',
+        '<materia>/<int:anio>/<cuatrimestre>/practicas/<int:numero_practica>/<int:numero>/nuevaSolucion/',
         soluciones.CrearSolucion.as_view(),
         name='solucion_practica'
     ),
     path(
-        '<materia>/<int:anio>/<cuatrimestre>/parciales/<int:numero_parcial>/<int:numero>/nuevaSolucion',
+        '<materia>/<int:anio>/<cuatrimestre>/parciales/<int:numero_parcial>/<int:numero>/nuevaSolucion/',
         soluciones.CrearSolucion.as_view(),
         {'es_recuperatorio': False},
         name='solucion_parcial'
     ),
     path(
-        '<materia>/<int:anio>/<cuatrimestre>/recuperatorios/<int:numero_parcial>/<int:numero>/nuevaSolucion',
+        '<materia>/<int:anio>/<cuatrimestre>/recuperatorios/<int:numero_parcial>/<int:numero>/nuevaSolucion/',
         soluciones.CrearSolucion.as_view(),
         {'es_recuperatorio': True},
         name='solucion_recuperatorio'
     ),
     path(
-        '<materia>/finales/<int:anio>/<int:mes>/<int:dia>/<int:numero>/nuevaSolucion',
+        '<materia>/finales/<int:anio>/<int:mes>/<int:dia>/<int:numero>/nuevaSolucion/',
         soluciones.CrearSolucion.as_view(),
         name='solucion_final'
     ),
 
     path(
-        '<materia>/agregarEnunciado',
+        '<materia>/agregarEnunciado/',
         enunciados.CrearEnunciado.as_view(),
         name='agregar_enunciado'
     ),
