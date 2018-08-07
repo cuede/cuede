fae75f776bf007a6549831ba4ac3122958d28ed7
Eric Brandwein <brandweineric@gmail.com>
2018-08-07 01:13:12 -0300

    Agrega link a agregar solución para el enunciado

--- secomplican/enunciados/views/enunciados/enunciados.py
+++ secomplican/enunciados/views/enunciados/enunciados.py
@@ -1,33 +1,43 @@
-from django.views.generic.edit import CreateView
 from django.forms import ModelForm
 from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.views.generic.edit import CreateView
 
 from enunciados import cuatrimestres_url_parser
 from enunciados.models import Enunciado, VersionTextoEnunciado
 from . import enunciados_utils
 
 
-def render_enunciado(request, enunciado_elegido):
-    contexto = {'enunciado': enunciado_elegido}
+def render_enunciado(request, enunciado_elegido, url_solucion):
+    contexto = {'enunciado': enunciado_elegido, 'url_solucion': url_solucion}
     return render(request, 'enunciados/enunciado.html', contexto)
 
 
 def enunciado_practica(request, materia, anio, cuatrimestre, numero_practica, numero):
     numero_cuatrimestre = cuatrimestres_url_parser.url_a_numero(cuatrimestre)
     encontrado = enunciados_utils.enunciado_de_practica(materia, anio, numero_cuatrimestre, numero_practica, numero)
-    return render_enunciado(request, encontrado)
+    url_solucion = reverse('solucion_practica', kwargs={
+        'materia': materia, 'anio': anio, 'cuatrimestre': cuatrimestre,
+        'numero_practica': numero_practica,
+        'numero': numero})
+    return render_enunciado(request, encontrado, url_solucion)
 
 
 def enunciado_parcial(request, materia, anio, cuatrimestre, numero_parcial, numero, es_recuperatorio):
     numero_cuatrimestre = cuatrimestres_url_parser.url_a_numero(cuatrimestre)
     encontrado = enunciados_utils.enunciado_de_parcial(
         materia, anio, numero_cuatrimestre, numero_parcial, numero, es_recuperatorio)
-    return render_enunciado(request, encontrado)
+    url_solucion = reverse('solucion_parcial', kwargs={
+        'materia': materia, 'anio': anio, 'cuatrimestre': cuatrimestre,
+        'numero_parcial': numero_parcial, 'numero': numero, 'es_recuperatorio': es_recuperatorio})
+    return render_enunciado(request, encontrado, url_solucion)
 
 
 def enunciado_final(request, materia, anio, mes, dia, numero):
     encontrado = enunciados_utils.enunciado_de_final(materia, anio, mes, dia, numero)
-    return render_enunciado(request, encontrado)
+    url_solucion = reverse('solucion_final', kwargs={
+        'materia': materia, 'anio': anio, 'mes': mes, 'dia': dia, 'numero': numero})
+    return render_enunciado(request, encontrado, url_solucion)
 
 
 class VersionTextoForm(ModelForm):
