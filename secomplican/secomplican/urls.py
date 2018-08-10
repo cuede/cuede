e7a2673f3c113fd0e6295897bc05375fc0664302
nsm <smnicolas>
2018-08-10 20:57:50 -0300

    Agrego modulo de django para loguear

--- secomplican/secomplican/urls.py
+++ secomplican/secomplican/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('enunciados.urls')),
 ]
+
+# urlpatterns += [
+
+# ]
+         
