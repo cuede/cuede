c82b131c37c7b715d06218a21abdb6b83839a07b
Eric Brandwein <brandweineric@gmail.com>
2018-08-10 18:07:44 -0300

    Adapta enunciados_utils para que no use Cuatrimestre

--- secomplican/enunciados/views/enunciados/enunciados_utils.py
+++ secomplican/enunciados/views/enunciados/enunciados_utils.py
@@ -9,23 +9,23 @@ def enunciado_de_practica(materia, anio, numero_cuatrimestre, numero_practica, n
         conjunto__materia__nombre=materia,
         numero=numero,
         conjunto__practica__isnull=False,
-        conjunto__practica__cuatrimestre__anio=anio,
-        conjunto__practica__cuatrimestre__numero=numero_cuatrimestre,
+        conjunto__practica__anio=anio,
+        conjunto__practica__cuatrimestre=numero_cuatrimestre,
         conjunto__practica__numero=numero_practica,
     )
 
 
 def enunciado_de_parcial(materia, anio, numero_cuatrimestre, numero_parcial, numero, es_recuperatorio):
     return get_object_or_404(
-            Enunciado,
-            conjunto__materia__nombre=materia,
-            numero=numero,
-            conjunto__parcial__isnull=False,
-            conjunto__parcial__cuatrimestre__anio=anio,
-            conjunto__parcial__cuatrimestre__numero=numero_cuatrimestre,
-            conjunto__parcial__numero=numero_parcial,
-            conjunto__parcial__recuperatorio=es_recuperatorio,
-        )
+        Enunciado,
+        conjunto__materia__nombre=materia,
+        numero=numero,
+        conjunto__parcial__isnull=False,
+        conjunto__parcial__anio=anio,
+        conjunto__parcial__cuatrimestre=numero_cuatrimestre,
+        conjunto__parcial__numero=numero_parcial,
+        conjunto__parcial__recuperatorio=es_recuperatorio,
+    )
 
 
 def enunciado_de_final(materia, anio, mes, dia, numero):
