7e8c8ef2318bcbc471cfaca1fdce531b2bbdca4b
nsm <smnicolas>
2018-08-10 21:42:06 -0300

    Agrego vista para signupear
    Falta casi todo, obvio, pero fundamentalmente (todo) validacion de
    mail

--- secomplican/secomplican/settings.py
+++ secomplican/secomplican/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,4 @@ STATICFILES_DIRS = (
 )
 
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
