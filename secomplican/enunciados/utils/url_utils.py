4fbbf243a23a2e35fd2d707052dd84abc8dc69e3
Eric Brandwein <brandweineric@gmail.com>
2018-08-15 21:43:22 -0300

    Arregla url_utils para que use los items de kwargs
    Por estas cosas necesitamos tests en realidad, pero paja.

--- secomplican/enunciados/utils/url_utils.py
+++ secomplican/enunciados/utils/url_utils.py
@@ -7,7 +7,7 @@ def add_query_params(url, **kwargs):
     def parametrizar(tupla):
         return '{key}={value}'.format(key=tupla[0], value=tupla[1])
 
-    parametros = map(parametrizar, kwargs)
+    parametros = map(parametrizar, kwargs.items())
     parametros_unidos = '&'.join(parametros)
     if parametros_unidos:
         url += '?' + parametros_unidos
