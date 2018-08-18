afcca4d4646cc21be8c4b2f9267554b252711754
Eric Brandwein <brandweineric@gmail.com>
2018-08-18 01:21:38 -0300

    Agrega field optativa a Materia

--- secomplican/enunciados/models.py
+++ secomplican/enunciados/models.py
@@ -12,6 +12,7 @@ class Materia(models.Model):
     NOMBRE_MAX_LENGTH = 1023
     nombre = models.CharField(max_length=NOMBRE_MAX_LENGTH)
     slug = models.SlugField(max_length=NOMBRE_MAX_LENGTH, unique=True)
+    optativa = models.BooleanField(default=False)
 
     def __str__(self):
         return self.nombre
