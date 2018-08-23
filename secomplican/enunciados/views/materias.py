41719e257f2365f6aae1ba1d35f983975c1d72bd
Eric Brandwein <brandweineric@gmail.com>
2018-08-23 18:05:51 -0300

    Agrega link a agregar ejercicio en vista de materia

--- secomplican/enunciados/views/materias.py
+++ secomplican/enunciados/views/materias.py
@@ -1,20 +1,38 @@
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 from django.views import generic
 
-from enunciados.utils import models_utils
 from enunciados.models import Materia
+from enunciados.utils import models_utils, url_utils
+from enunciados.views.enunciados.forms import ConjuntoDeEnunciadosForm
 
 
 class MateriasView(generic.ListView):
     model = Materia
 
 
+def url_agregar_conjunto(slug_materia, tipo):
+    url = reverse('agregar_enunciado', kwargs={'materia': slug_materia})
+    return url_utils.add_query_params(url, tipo=tipo)
+
+
 def materia(request, nombre):
     # El nombre es un slug.
+    materia_encontrada = get_object_or_404(Materia, slug=nombre)
+    tipo_practica = ConjuntoDeEnunciadosForm.PRACTICA
+    tipo_parcial = ConjuntoDeEnunciadosForm.PARCIAL
+    tipo_final = ConjuntoDeEnunciadosForm.FINAL
     contexto = {
-        'materia': get_object_or_404(Materia, slug=nombre),
+        'materia': materia_encontrada,
+        'practicas': models_utils.ultimas_practicas_ordenadas(
+            materia_encontrada),
+        'parciales': models_utils.parciales_de_materia_ordenados(
+            materia_encontrada),
+        'finales': models_utils.finales_de_materia_ordenados(
+            materia_encontrada),
+
+        'url_agregar_practica': url_agregar_conjunto(nombre, tipo_practica),
+        'url_agregar_parcial': url_agregar_conjunto(nombre, tipo_parcial),
+        'url_agregar_final': url_agregar_conjunto(nombre, tipo_final),
     }
-    contexto['practicas'] = models_utils.ultimas_practicas_ordenadas(contexto['materia'])
-    contexto['parciales'] = models_utils.parciales_de_materia_ordenados(contexto['materia'])
-    contexto['finales'] = models_utils.finales_de_materia_ordenados(contexto['materia'])
     return render(request, 'enunciados/materia.html', contexto)
