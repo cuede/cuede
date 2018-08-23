43fad8f425ffed616323f0fda49344aae99ac8db
Eric Brandwein <brandweineric@gmail.com>
2018-08-23 20:01:00 -0300

    Mueve templates de registracion a app accounts

--- secomplican/accounts/views.py
+++ secomplican/accounts/views.py
@@ -8,4 +8,4 @@ from django.views import generic
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+    template_name = 'registration/signup.html'
