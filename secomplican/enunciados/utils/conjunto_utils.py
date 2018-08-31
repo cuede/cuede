13db9fbf3e1fbf9a90a8b4ff4c4f294e098ad721
Eric Brandwein <brandweineric@gmail.com>
2018-08-30 23:34:14 -0300

    Agrega tipo_conjunto_a_url a conjunto_utils.py

--- secomplican/enunciados/utils/conjunto_utils.py
+++ secomplican/enunciados/utils/conjunto_utils.py
@@ -1,21 +1,36 @@
 from enunciados.models import Practica, Parcial, Final
 
+
+def tipo_conjunto_a_url(conjunto):
+    if isinstance(conjunto, Practica):
+        return 'practica'
+    elif isinstance(conjunto, Parcial):
+        if conjunto.recuperatorio:
+            return 'recuperatorio'
+        else:
+            return 'parcial'
+    elif isinstance(conjunto, Final):
+        return 'final'
+    else:
+        # No debería pasar nunca.
+        return None
+
+
 def tipo_de_conjunto(conjunto):
     """Devuelve el tipo del conjunto del conjunto pasado."""
     # Esto es horrible, pero no sé si hay otra forma de
     # chequear de qué subtipo es el conjunto.
     try:
-        practica = conjunto.practica
-        return 'practica'
+        conjunto = conjunto.practica
     except Practica.DoesNotExist:
         try:
-            parcial = conjunto.parcial
-            return 'parcial'
+            conjunto = conjunto.parcial
         except Parcial.DoesNotExist:
             try:
-                final = conjunto.final
-                return 'final'
+                conjunto = conjunto.final
             except Final.DoesNotExist:
                 # No debería pasar nunca.
                 return None
+
+    return tipo_conjunto_a_url(conjunto)
 
