c0f4ebc0e88c6f38d770e45d7563a50584150ff6
Eric Brandwein <brandweineric@gmail.com>
2018-09-04 19:13:48 -0300

    Volver a versión anterior en solución

--- secomplican/enunciados/views/soluciones/versiones.py
+++ secomplican/enunciados/views/soluciones/versiones.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 
 from enunciados.models import Solucion
 
+
+def volver_a_version(objeto, pk):
+    version = get_object_or_404(objeto.versiones, pk=pk)
+    version.pk = None
+    version.save()
+
+
 def versiones_solucion(request, pk):
     solucion = get_object_or_404(Solucion, pk=pk)
+    if request.method == 'POST':
+        # Nos postearon una versión a la que volver.
+        # La versión está en version_pk.
+        pk = request.POST.get('version_pk')
+        if pk:
+            volver_a_version(solucion, pk)
+            return redirect(solucion.enunciado)
+
     return render(
         request, 'enunciados/versiones_solucion.html', {'solucion': solucion})
