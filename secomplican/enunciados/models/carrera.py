0073b0bb659159a439f4ada535135bacc8e814c5
Eric Brandwein <brandweineric@gmail.com>
2018-09-13 23:50:58 -0300

    Hace que la relación Carrera-Materia sea un ManyToMany

--- secomplican/enunciados/models/carrera.py
+++ secomplican/enunciados/models/carrera.py
@@ -17,12 +17,13 @@ class Universidad(models.Model):
 class Carrera(models.Model):
     nombre = models.CharField()
     universidad = models.ForeignKey(Universidad, on_delete=models.CASCADE)
+    materias = models.ManyToManyField(Materia, through='MateriaCarrera')
 
     def __str__(self):
         return self.nombre
 
 
-class CarreraMateria(models.Model):
+class MateriaCarrera(models.Model):
     carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
     materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=NOMBRE_MAX_LENGTH)
