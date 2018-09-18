8a0cc1200b5881ff0fbe8ee84a1a95728afc2dea
Eric Brandwein <brandweineric@gmail.com>
2018-09-18 19:16:31 -0300

    Crea utils.conjuntos_url_parser.url_practica

---
+++ secomplican/enunciados/utils/conjuntos_url_parser.py
@@ -0,0 +1,22 @@
+from django.urls import reverse
+
+from enunciados.utils import cuatrimestres_url_parser
+
+
+def url_conjunto(materiacarrera, nombre, kwargs):
+    kwargs['materia_carrera'] = materiacarrera
+    return reverse('nombre', kwargs=kwargs)
+
+
+def url_conjunto_con_cuatrimestre(materiacarrera, nombre, kwargs, conjunto):
+    kwargs['anio'] = conjunto.anio
+    kwargs['cuatrimestre'] = conjunto.cuatrimestre
+    return url_conjunto(materiacarrera, nombre, kwargs)
+
+
+def url_practica(materiacarrera, practica):
+    kwargs = {
+        'numero_practica': practica.numero,
+    }
+    return url_conjunto_con_cuatrimestre(
+        materiacarrera, 'practica', kwargs, practica)
