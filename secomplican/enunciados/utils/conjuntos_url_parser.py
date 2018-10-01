87551d25e6a85240c324570bba6774fece94dca4
Eric Brandwein <brandweineric@gmail.com>
2018-09-30 23:11:32 -0300

    Crea conjuntos_url_parser.nombre_url_conjunto()

--- secomplican/enunciados/utils/conjuntos_url_parser.py
+++ secomplican/enunciados/utils/conjuntos_url_parser.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 
-from enunciados.utils import cuatrimestres_url_parser
+from enunciados.utils import cuatrimestres_url_parser, conjuntos_utils
 from enunciados.models import Practica, Parcial, Final
 
 
@@ -30,26 +30,34 @@ def kwargs_de_final(final):
     }
 
 
-def url_conjunto(materiacarrera, nombre, kwargs):
+def nombre_url_conjunto(materiacarrera, conjunto):
+    tipo_conjunto = conjuntos_utils.tipo_conjunto(conjunto)
+    nombre = tipo_conjunto
+    if tipo_conjunto == 'parcial':
+        subdomain = \
+            'recuperatorios' if conjunto.parcial.recuperatorio else 'parciales'
+        nombre = '{}:{}'.format(subdomain, nombre)
+    return 'materia:{}'.format(nombre)
+
+
+def url_conjunto(materiacarrera, conjunto, kwargs):
     kwargs['materia_carrera'] = materiacarrera
-    nombre = 'materia:{}'.format(nombre)
+    nombre = nombre_url_conjunto(materiacarrera, conjunto)
     return reverse(nombre, kwargs=kwargs)
 
 
 def url_practica(materiacarrera, practica):
     kwargs = kwargs_de_practica(practica)
-    return url_conjunto(materiacarrera, 'practica', kwargs)
+    return url_conjunto(materiacarrera, practica, kwargs)
 
 
 def url_parcial(materiacarrera, parcial):
-    subdomain = 'recuperatorios' if parcial.recuperatorio else 'parciales'
-    nombre = '{}:parcial'.format(subdomain)
     kwargs = kwargs_de_parcial(parcial)
-    return url_conjunto(materiacarrera, nombre, kwargs)
+    return url_conjunto(materiacarrera, parcial, kwargs)
 
 
 def url_final(materiacarrera, final):
-    return url_conjunto(materiacarrera, 'final', kwargs_de_final(final))
+    return url_conjunto(materiacarrera, final, kwargs_de_final(final))
 
 
 def kwargs_a_conjunto(kwargs):
