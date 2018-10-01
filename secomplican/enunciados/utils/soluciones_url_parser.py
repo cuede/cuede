2a2ba882db841cbe11c5adda5167403045cf1ef8
Eric Brandwein <brandweineric@gmail.com>
2018-10-01 00:44:00 -0300

    Hace que soluciones_url_parser use el nuevo formato de url

--- secomplican/enunciados/utils/soluciones_url_parser.py
+++ secomplican/enunciados/utils/soluciones_url_parser.py
@@ -1,38 +1,8 @@
 from django.urls import reverse
 
-from enunciados.utils import conjuntos_url_parser, conjuntos_utils
-
-
-def _nombre_url_nueva_solucion(enunciado):
-    tipo_conjunto = conjuntos_utils.tipo_conjunto(enunciado.conjunto)
-    if tipo_conjunto == 'practica':
-        namespace = 'enunciados_practica'
-    elif tipo_conjunto == 'parcial':
-        if enunciado.parcial.recuperatorio:
-            namespace = 'recuperatorios:enunciados_parcial'
-        else:
-            namespace = 'parciales:enunciados_parcial'
-    elif tipo_conjunto == 'final':
-        namespace = 'enunciados_final'
-
-    return ':'.join(['materia', namespace, 'crear_solucion'])
+from enunciados.utils import enunciados_url_parser
 
 
 def url_nueva_solucion(materia_carrera, enunciado):
-    nombre_url = _nombre_url_nueva_solucion(enunciado)
-
-    tipo_conjunto = conjuntos_utils.tipo_conjunto(enunciado.conjunto)
-    kwargs = {}
-    if tipo_conjunto == 'practica':
-        kwargs = conjuntos_url_parser.kwargs_de_practica(
-            enunciado.conjunto.practica)
-    elif tipo_conjunto == 'parcial':
-        kwargs = conjuntos_url_parser.kwargs_de_parcial(
-            enunciado.conjunto.parcial)
-    elif tipo_conjunto == 'final':
-        kwargs = conjuntos_url_parser.kwargs_de_final(
-            enunciado.conjunto.final)
-
-    kwargs['materia_carrera'] = materia_carrera
-    kwargs['numero'] = enunciado.numero
-    return reverse(nombre_url, kwargs=kwargs)
+    return enunciados_url_parser.url_enunciado_con_nombre(
+        materia_carrera, enunciado, 'crear_solucion')
