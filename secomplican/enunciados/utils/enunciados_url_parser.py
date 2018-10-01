467fb30b9bc2d16559d0b8867f7fd16cea6bbfca
Eric Brandwein <brandweineric@gmail.com>
2018-09-30 23:27:49 -0300

    Agrega enunciados_url_parser.url_enunciado()

---
+++ secomplican/enunciados/utils/enunciados_url_parser.py
@@ -0,0 +1,13 @@
+from django.urls import reverse
+
+from enunciados.utils import conjuntos_url_parser
+
+
+def url_enunciado(materia_carrera, enunciado):
+    nombre_url_conjunto = conjuntos_url_parser.nombre_url_conjunto(
+        materia_carrera, enunciado.conjunto)
+    nombre_url_enunciado = '{}:ver_enunciado'.format(nombre_url_conjunto)
+    kwargs = conjuntos_url_parser.kwargs_de_conjunto(
+        materia_carrera, enunciado.conjunto)
+    kwargs['numero'] = enunciado.numero
+    return reverse('ver_enunciado', kwargs=kwargs)
