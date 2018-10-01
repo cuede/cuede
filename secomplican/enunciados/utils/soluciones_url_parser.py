6ce72beaf22bfc4dad621afcc333c8e5979d50e3
Eric Brandwein <brandweineric@gmail.com>
2018-10-01 00:55:46 -0300

    Agrega soluciones_url_parser.url_editar_solucion()

--- secomplican/enunciados/utils/soluciones_url_parser.py
+++ secomplican/enunciados/utils/soluciones_url_parser.py
@@ -6,3 +6,16 @@ from enunciados.utils import enunciados_url_parser
 def url_nueva_solucion(materia_carrera, enunciado):
     return enunciados_url_parser.url_enunciado_con_nombre(
         materia_carrera, enunciado, 'crear_solucion')
+
+
+def kwargs_de_solucion(materia_carrera, solucion):
+    return {
+        'carrera': materia_carrera.carrera,
+        'materia_carrera': materia_carrera,
+        'pk': solucion.pk,
+    }
+
+
+def url_editar_solucion(materia_carrera, solucion):
+    kwargs = kwargs_de_solucion(materia_carrera, solucion)
+    return reverse('editar_solucion', kwargs=kwargs)
