e5722e2e97e17c647ae6845e83b1511bb874549f
Eric Brandwein <brandweineric@gmail.com>
2018-09-30 23:21:08 -0300

    Agrega conjuntos_url_parser.kwargs_de_conjunto()

--- secomplican/enunciados/utils/conjuntos_url_parser.py
+++ secomplican/enunciados/utils/conjuntos_url_parser.py
@@ -30,6 +30,21 @@ def kwargs_de_final(final):
     }
 
 
+def kwargs_de_conjunto(materiacarrera, conjunto):
+    tipo_conjunto = conjuntos_utils.tipo_conjunto(conjunto)
+    if tipo_conjunto == 'practica':
+        kwargs = kwargs_de_practica(conjunto.practica)
+    elif tipo_conjunto == 'parcial':
+        kwargs = kwargs_de_parcial(conjunto.parcial)
+    elif tipo_conjunto == 'final':
+        kwargs = kwargs_de_final(conjunto.final)
+    else:
+        raise ValueError('Tipo de conjunto no conocido.')
+
+    kwargs['materia_carrera'] = materiacarrera
+    return kwargs
+
+
 def nombre_url_conjunto(materiacarrera, conjunto):
     tipo_conjunto = conjuntos_utils.tipo_conjunto(conjunto)
     nombre = tipo_conjunto
@@ -40,24 +55,22 @@ def nombre_url_conjunto(materiacarrera, conjunto):
     return 'materia:{}'.format(nombre)
 
 
-def url_conjunto(materiacarrera, conjunto, kwargs):
-    kwargs['materia_carrera'] = materiacarrera
+def url_conjunto(materiacarrera, conjunto):
+    kwargs = kwargs_de_conjunto(materiacarrera, conjunto)
     nombre = nombre_url_conjunto(materiacarrera, conjunto)
     return reverse(nombre, kwargs=kwargs)
 
 
 def url_practica(materiacarrera, practica):
-    kwargs = kwargs_de_practica(practica)
-    return url_conjunto(materiacarrera, practica, kwargs)
+    return url_conjunto(materiacarrera, practica)
 
 
 def url_parcial(materiacarrera, parcial):
-    kwargs = kwargs_de_parcial(parcial)
-    return url_conjunto(materiacarrera, parcial, kwargs)
+    return url_conjunto(materiacarrera, parcial)
 
 
 def url_final(materiacarrera, final):
-    return url_conjunto(materiacarrera, final, kwargs_de_final(final))
+    return url_conjunto(materiacarrera, final)
 
 
 def kwargs_a_conjunto(kwargs):
