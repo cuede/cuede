be87a2d99cd39517365e7922b3bca1097fc0a6b6
Eric Brandwein <brandweineric@gmail.com>
2018-10-01 00:06:45 -0300

    Crea enunciados_url_parser.namespace_enunciado()

--- secomplican/enunciados/utils/enunciados_url_parser.py
+++ secomplican/enunciados/utils/enunciados_url_parser.py
@@ -3,11 +3,15 @@ from django.urls import reverse
 from enunciados.utils import conjuntos_url_parser
 
 
-def url_enunciado(materia_carrera, enunciado):
+def namespace_enunciado(enunciado):
     namespace_conjunto = conjuntos_url_parser.namespace_de_conjunto(
         enunciado.conjunto)
-    nombre_url_enunciado = '{}:enunciados:ver_enunciado'.format(
-        namespace_conjunto)
+    return '{}:enunciados'.format(namespace_conjunto)
+
+
+def url_enunciado(materia_carrera, enunciado):
+    namespace = namespace_enunciado(enunciado)
+    nombre_url_enunciado = '{}:ver_enunciado'.format(namespace)
     kwargs = conjuntos_url_parser.kwargs_de_conjunto(
         materia_carrera, enunciado.conjunto)
     kwargs['numero'] = enunciado.numero
