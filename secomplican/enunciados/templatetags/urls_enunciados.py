ac973be944d4540a75f982ffe1d5bbb44ec4f38f
Eric Brandwein <brandweineric@gmail.com>
2018-10-01 00:17:34 -0300

    Crea template tag de url_editar_enunciado

--- secomplican/enunciados/templatetags/urls_enunciados.py
+++ secomplican/enunciados/templatetags/urls_enunciados.py
@@ -5,3 +5,4 @@ from enunciados.utils import enunciados_url_parser
 register = template.Library()
 
 register.simple_tag(enunciados_url_parser.url_enunciado)
+register.simple_tag(enunciados_url_parser.url_editar_enunciado)
