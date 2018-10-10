c0b7ee69b1815624473fb90fcb7baf65142fbe2e
Eric Brandwein <brandweineric@gmail.com>
2018-10-10 16:38:26 -0300

    Agrega conjuntos_utils.tipo_a_clase
    Para poder obtener la clase del conjunto según el tipo.

--- secomplican/enunciados/utils/conjuntos_utils.py
+++ secomplican/enunciados/utils/conjuntos_utils.py
@@ -1,19 +1,39 @@
 from django.core.exceptions import ObjectDoesNotExist
 
+from enunciados.models import Practica, Parcial, Final
+
+TIPO_PRACTICA = 'practica'
+TIPO_PARCIAL = 'parcial'
+TIPO_FINAL = 'final'
+
+__tipo_a_clase = {
+    TIPO_PRACTICA: Practica,
+    TIPO_PARCIAL: Parcial,
+    TIPO_FINAL: Final,
+}
+
 
 def tipo_conjunto(conjunto):
     try:
         conjunto.practica
-        tipo = 'practica'
+        tipo = TIPO_PRACTICA
     except ObjectDoesNotExist:
         try:
             conjunto.parcial
-            tipo = 'parcial'
+            tipo = TIPO_PARCIAL
         except ObjectDoesNotExist:
             try:
                 conjunto.final
-                tipo = 'final'
+                tipo = TIPO_FINAL
             except ObjectDoesNotExist:
                 raise ValueError('Tipo de conjunto no conocido')
 
     return tipo
+
+
+def tipo_a_clase(tipo):
+    """
+    Convierte un string de tipo devuelto por tipo_conjunto
+    a la clase correspondiente.
+    """
+    return __tipo_a_clase[tipo]
