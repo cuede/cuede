5991e85a74b4a977e4da263256dce913c67b4439
Eric Brandwein <brandweineric@gmail.com>
2018-10-11 00:24:39 -0300

    Agrega URLs de crear conjuntos

--- secomplican/enunciados/urls/materia_urls.py
+++ secomplican/enunciados/urls/materia_urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-from enunciados.views import materias
+from enunciados.views import materias, conjuntos_de_enunciados
 
 app_name = 'materia'
 
@@ -23,5 +23,20 @@ urlpatterns = [
     path(
         'finales/',
         include('enunciados.urls.finales_urls', namespace='finales')
+    ),
+    path(
+        'agregar-practica/',
+        conjuntos_de_enunciados.CrearPracticaView.as_view(),
+        name='crear_practica',
+    ),
+    path(
+        'agregar-parcial/',
+        conjuntos_de_enunciados.CrearParcialView.as_view(),
+        name='crear_parcial',
+    ),
+    path(
+        'agregar-final/',
+        conjuntos_de_enunciados.CrearFinalView.as_view(),
+        name='crear_final',
     ),
 ]
