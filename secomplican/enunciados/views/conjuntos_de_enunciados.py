0367513b5e61facea567cafe32d7b33050086cbd
Eric Brandwein <brandweineric@gmail.com>
2018-10-11 00:24:24 -0300

    Agrega vistas de crear conjuntos

--- secomplican/enunciados/views/conjuntos_de_enunciados.py
+++ secomplican/enunciados/views/conjuntos_de_enunciados.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.urls import reverse
+from django.utils.translation import gettext as _
+from django.views.generic.edit import CreateView
+
+from enunciados.models import Final, Parcial, Practica
 from enunciados.utils import conjuntos_url_parser, enunciados_url_parser
+from enunciados.views import conjuntos_de_enunciados_forms
 
 
 def conjunto_de_enunciados(request, **kwargs):
@@ -15,3 +19,36 @@ def conjunto_de_enunciados(request, **kwargs):
         'url_nuevo_enunciado': url_nuevo_enunciado,
     }
     return render(request, 'enunciados/conjunto_de_enunciados.html', contexto)
+
+
+class CrearConjuntoDeEnunciadosView(CreateView):
+    template_name = 'enunciados/nuevo_conjunto.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = self.titulo
+        return context
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['materia'] = self.kwargs['materia_carrera'].materia
+        return kwargs
+
+    def get_success_url(self):
+        materia_carrera = self.kwargs['materia_carrera']
+        return conjuntos_url_parser.url_conjunto(materia_carrera, self.object)
+
+
+class CrearPracticaView(CrearConjuntoDeEnunciadosView):
+    form_class = conjuntos_de_enunciados_forms.PracticaForm
+    titulo = _('Nueva Práctica')
+
+
+class CrearParcialView(CrearConjuntoDeEnunciadosView):
+    form_class = conjuntos_de_enunciados_forms.ParcialForm
+    titulo = _('Nuevo Parcial')
+
+
+class CrearFinalView(CrearConjuntoDeEnunciadosView):
+    form_class = conjuntos_de_enunciados_forms.FinalForm
+    titulo = _('Nuevo Final')
