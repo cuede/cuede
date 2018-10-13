a05fef47cdbc9c3eac242f1eda4d5bc6953f30de
Eric Brandwein <brandweineric@gmail.com>
2018-10-13 03:20:05 -0300

    Arregla tests_conjuntos_de_enunciados_forms

--- secomplican/enunciados/tests/views/conjuntos_de_enunciados/tests_conjuntos_de_enunciados_forms.py
+++ secomplican/enunciados/tests/views/conjuntos_de_enunciados/tests_conjuntos_de_enunciados_forms.py
@@ -1,24 +1,36 @@
 from django.test import TestCase
 
-from enunciados.models import Materia, Parcial, Practica
+from enunciados.models import (
+    Universidad, Carrera, MateriaCarrera, Materia, Parcial, Practica
+)
 from enunciados.views.conjuntos_de_enunciados.conjuntos_de_enunciados_forms import (
     ParcialForm, PracticaForm
 )
 
 
+def crear_materia_carrera():
+    universidad = Universidad.objects.create()
+    carrera = Carrera.objects.create(
+        universidad=universidad, nombre='carrera', slug='carrera')
+    materia = Materia.objects.create()
+    return MateriaCarrera.objects.create(
+        materia=materia, carrera=carrera, nombre='materia', slug='materia'
+    )
+
+
 class PracticaFormTests(TestCase):
     def test_guardar_practica_ya_existente(self):
         """Debería no guardarla."""
-        materia = Materia.objects.create()
+        materia_carrera = crear_materia_carrera()
         anio = 2018
         cuatrimestre = 1
         numero = 1
         practica_existente = Practica.objects.create(
-            materia=materia, anio=2018,
+            materia=materia_carrera.materia, anio=2018,
             cuatrimestre=cuatrimestre, numero=numero
         )
         form = PracticaForm(
-            materia,
+            materia_carrera,
             {
                 'anio': anio,
                 'cuatrimestre': cuatrimestre,
@@ -33,16 +45,16 @@ class PracticaFormTests(TestCase):
 class ParcialFormTest(TestCase):
     def test_guardar_parcial_ya_existente(self):
         """Debería no guardarlo."""
-        materia = Materia.objects.create()
+        materia_carrera = crear_materia_carrera()
         anio = 2018
         cuatrimestre = 1
         numero = 1
         parcial_existente = Parcial.objects.create(
-            materia=materia, anio=2018,
+            materia=materia_carrera.materia, anio=2018,
             cuatrimestre=cuatrimestre, numero=numero
         )
         form = ParcialForm(
-            materia,
+            materia_carrera,
             {
                 'anio': anio,
                 'cuatrimestre': cuatrimestre,
