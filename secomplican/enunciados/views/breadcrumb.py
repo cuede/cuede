a2aa500de23b9ff12f6e4a55031530ca3efcdb12
Eric Brandwein <brandweineric@gmail.com>
2019-01-02 17:38:38 -0300

    Muestra breadcrumb en vista de enunciado

--- secomplican/enunciados/views/breadcrumb.py
+++ secomplican/enunciados/views/breadcrumb.py
@@ -1,5 +1,8 @@
 from django.urls import reverse
 
+from enunciados.utils import conjuntos_utils
+from enunciados.utils import conjuntos_url_parser
+
 
 class BreadcrumbPage:
     def __init__(self, title, url=None):
@@ -20,5 +23,15 @@ def breadcrumb_materia(materia_carrera):
 
 def breadcrumb_conjunto_de_enunciados(materia_carrera, conjunto):
     return breadcrumb_materia(materia_carrera) + [
-        BreadcrumbPage(conjunto)
+        BreadcrumbPage(
+            conjunto,
+            conjuntos_url_parser.url_conjunto(materia_carrera, conjunto)
+        )
     ]
+
+
+def breadcrumb_ver_enunciado(materia_carrera, enunciado):
+    breadcrumb_conjunto = breadcrumb_conjunto_de_enunciados(
+        materia_carrera, conjuntos_utils.castear_a_subclase(enunciado.conjunto)
+    )
+    return breadcrumb_conjunto + [BreadcrumbPage(enunciado)]
