94bddbf7a8467e0657a5a859154398169d7ba869
Eric Brandwein <brandweineric@gmail.com>
2019-01-14 16:14:33 -0300

    Labels de form de crear enunciado

--- secomplican/enunciados/views/enunciados/forms.py
+++ secomplican/enunciados/views/enunciados/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
-
+from django.utils.translation import gettext_lazy
 from enunciados.models import Enunciado, VersionTextoEnunciado
 
 
@@ -23,6 +23,9 @@ class EnunciadoConConjuntoForm(forms.ModelForm):
     class Meta:
         model = Enunciado
         fields = ['numero']
+        labels = {
+            'numero': gettext_lazy('Número')
+        }
 
 
 class VersionTextoForm(forms.ModelForm):
