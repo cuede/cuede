e9e5833605914e2c17b5967c33375571fd93150e
Eric Brandwein <brandweineric@gmail.com>
2019-01-17 00:18:24 -0300

    Quill en vista de crear enunciado

---
+++ secomplican/enunciados/static/enunciados/editor.js
@@ -0,0 +1,34 @@
+var quill = new Quill('#editor', {
+    theme: 'snow',
+    formats: ['code', 'formula'],
+    modules: {
+        toolbar: '#toolbar'
+    },
+});
+
+function surroundSelectionBy(before, after) {
+    var selection = quill.getSelection();
+    if (selection) {
+        quill.insertText(selection.index + selection.length, after)
+        quill.insertText(selection.index, before);
+        quill.setSelection(selection.index + before.length, selection.length)
+    }
+}
+
+function toolbarCodeHandler() {
+    surroundSelectionBy('```\n', '\n```');
+}
+
+function toolbarFormulaHandler() {
+    surroundSelectionBy('$$\n', '\n$$');
+}
+
+var toolbar = quill.getModule('toolbar');
+toolbar.addHandler('code', toolbarCodeHandler);
+toolbar.addHandler('formula', toolbarFormulaHandler);
+
+var textElement = document.getElementById('hidden_textarea');
+var form = document.getElementById('form');
+form.onsubmit = function () {
+    textElement.innerHTML = quill.getText();
+}
