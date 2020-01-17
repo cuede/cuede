187d46dcc8db1ad541e647f02a503ea381f6ccd7
Eric Brandwein <brandweineric@gmail.com>
2020-01-17 15:19:09 -0300

    Editar soluciones da 5 puntos

---
+++ secomplican/enunciados/tests/views/soluciones/tests_editar_soluciones.py
@@ -0,0 +1,119 @@
+from http import HTTPStatus
+
+from django.test import TestCase
+from django.urls import reverse
+
+from django.contrib.auth import get_user_model
+
+from enunciados.models import (
+    Universidad, Carrera, Materia, MateriaCarrera, Practica, Enunciado, Solucion, VersionTexto
+)
+from enunciados.views.soluciones.editar import PUNTOS_USUARIO_POR_EDITAR_SOLUCION
+
+class EditarSolucionTests(TestCase):
+    def setUp(self):
+        universidad = Universidad.objects.create(nombre='uba')
+        carrera = Carrera.objects.create(
+            nombre='Computación', slug='compu', universidad=universidad)
+        materia = Materia.objects.create()
+        self.materia_carrera = MateriaCarrera.objects.create(
+            nombre='materia', slug='materia', carrera=carrera, materia=materia
+        )
+        practica = Practica.objects.create(
+            materia=materia, anio=2018, cuatrimestre=1, numero=1
+        )
+        self.enunciado = Enunciado.objects.create(conjunto=practica, numero=1)
+        self.creador = get_user_model().objects.create_user(username='user', password='')
+        self.editor = get_user_model().objects.create_user(username='editor', password='')
+        self.solucion = Solucion.objects.create(
+            enunciado_padre=self.enunciado, creador=self.creador)
+        self.version_texto = VersionTexto.versiones.create(
+            texto='asdsad', posteo=self.solucion, autor=self.creador)
+
+    def url_editar_solucion(self, pk_solucion=None):
+        if not pk_solucion:
+            pk_solucion = self.solucion.pk
+        return reverse('materia:practicas:practica:enunciados:editar_solucion', kwargs={
+            'materia_carrera': self.materia_carrera,
+            'anio': 2018,
+            'cuatrimestre': 1,
+            'numero_practica': 1,
+            'numero': 1,
+            'pk_solucion': pk_solucion,
+        })
+
+    def assert_puntos_de_editor_son(self, puntos):
+        self.editor.refresh_from_db()
+        self.assertEquals(self.editor.informacionusuario.puntos, puntos)
+
+    def test_no_se_puede_editar_solucion_sin_loguearse(self):
+        url = self.url_editar_solucion()
+        response = self.client.post(url)
+
+        self.assertEquals(response.status_code, HTTPStatus.FOUND)
+        url_redir = reverse('login') + '?next=' + url
+        self.assertEquals(response.url, url_redir)
+        self.assertEquals(self.solucion.versiones.ultima(), self.version_texto)
+
+    def test_no_se_puede_hacer_get_a_editar_solucion_sin_loguearse(self):
+        url = self.url_editar_solucion()
+        response = self.client.get(url)
+
+        self.assertEquals(response.status_code, HTTPStatus.FOUND)
+        url_redir = reverse('login') + '?next=' + url
+        self.assertEquals(response.url, url_redir)
+        self.assertEquals(self.solucion.versiones.ultima(), self.version_texto)
+
+    def test_editar_solucion_da_puntos_al_usuario(self):
+        self.client.login(username=self.editor.username, password='')
+
+        self.client.post(self.url_editar_solucion(), {'texto': 'asdsdasdasd'})
+
+        self.assert_puntos_de_editor_son(PUNTOS_USUARIO_POR_EDITAR_SOLUCION)
+
+    def test_get_no_da_puntos_al_usuario(self):
+        self.client.login(username=self.editor.username, password='')
+
+        self.client.get(self.url_editar_solucion())
+
+        self.assert_puntos_de_editor_son(0)
+
+    def test_form_invalida_no_da_puntos_al_usuario(self):
+        self.client.login(username=self.editor.username, password='')
+
+        # No se cambia el texto
+        self.client.post(self.url_editar_solucion(), {'texto': self.version_texto.texto})
+
+        self.assert_puntos_de_editor_son(0)
+
+    def test_editar_solucion_no_da_puntos_si_creaste_esa_solucion(self):
+        self.client.login(username=self.creador.username, password='')
+
+        self.client.post(self.url_editar_solucion(), {'texto': 'testooooasljdlasdkls'})
+
+        self.creador.refresh_from_db()
+        self.assertEquals(self.creador.informacionusuario.puntos, 0)
+
+    def test_editar_dos_soluciones_da_puntos_por_cada_una(self):
+        self.client.login(username=self.editor.username, password='')
+
+        solucion2 = Solucion.objects.create(
+            enunciado_padre=self.enunciado, creador=self.creador)
+        version_texto2 = VersionTexto.versiones.create(
+            texto='asdsad', posteo=solucion2, autor=self.creador)
+
+        url = self.url_editar_solucion()
+        self.client.post(url, {'texto': 'testooooasljdlasdkls'})
+        url2 = self.url_editar_solucion(pk_solucion=solucion2.pk)
+        self.client.post(url2, {'texto': 'testooooasljdlasdkls'})
+
+        self.assert_puntos_de_editor_son(PUNTOS_USUARIO_POR_EDITAR_SOLUCION * 2)
+
+    def test_editar_solucion_no_da_puntos_si_ya_editaste_esa_solucion(self):
+        self.client.login(username=self.editor.username, password='')
+
+        self.client.post(self.url_editar_solucion(), {'texto': 'testooooasljdlasdkls'})
+        self.client.post(self.url_editar_solucion(), {'texto': 'otralakjsdlksajdlk'})
+
+        self.assert_puntos_de_editor_son(PUNTOS_USUARIO_POR_EDITAR_SOLUCION)
+
--- secomplican/enunciados/views/soluciones/editar.py
+++ secomplican/enunciados/views/soluciones/editar.py
@@ -1,15 +1,21 @@
 from django.forms import ModelForm, ValidationError
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.translation import gettext as _
+from django.contrib.auth.decorators import login_required
+from django.db.models import F
 
 from enunciados.models import Solucion, VersionTexto
 from enunciados.utils import enunciados_url_parser
 from enunciados.views.breadcrumb import breadcrumb_editar_solucion
 
 
+PUNTOS_USUARIO_POR_EDITAR_SOLUCION = 5
+
+
 class VersionTextoSolucionForm(ModelForm):
-    def __init__(self, solucion, *args, **kwargs):
+    def __init__(self, solucion, autor, *args, **kwargs):
         self.solucion = solucion
+        self.autor = autor
         super().__init__(*args, **kwargs)
 
     def clean_texto(self):
@@ -21,6 +27,7 @@ class VersionTextoSolucionForm(ModelForm):
 
     def save(self):
         self.instance.posteo = self.solucion
+        self.instance.autor = self.autor
         return super().save()
 
     class Meta:
@@ -28,19 +35,34 @@ class VersionTextoSolucionForm(ModelForm):
         fields = ['texto']
 
 
+def no_edito_solucion(solucion, usuario):
+    return solucion.versiones.filter(autor=usuario).count() == 0
+
+
+def dar_puntos_a_usuario(usuario):
+    informacion_usuario = usuario.informacionusuario
+    informacion_usuario.puntos = F('puntos') + PUNTOS_USUARIO_POR_EDITAR_SOLUCION
+    informacion_usuario.save()
+
+
+@login_required
 def editar_solucion(request, pk_solucion, **kwargs):
     materia_carrera = kwargs['materia_carrera']
     solucion = get_object_or_404(Solucion, pk=pk_solucion)
+    usuario = request.user
     if request.method == 'POST':
-        form = VersionTextoSolucionForm(solucion, request.POST)
+        form = VersionTextoSolucionForm(solucion, usuario, request.POST)
         if form.is_valid():
+            deberia_sumar_puntos = no_edito_solucion(solucion, usuario)
             form.save()
+            if deberia_sumar_puntos:
+                dar_puntos_a_usuario(usuario)
             success_url = enunciados_url_parser.url_enunciado(
                 materia_carrera, solucion.enunciado_padre)
             return redirect(success_url)
     else:
         form = VersionTextoSolucionForm(
-            solucion, initial={'texto': solucion.versiones.ultima().texto})
+            solucion, usuario, initial={'texto': solucion.versiones.ultima().texto})
 
     contexto = {
         'materia_carrera': materia_carrera,
