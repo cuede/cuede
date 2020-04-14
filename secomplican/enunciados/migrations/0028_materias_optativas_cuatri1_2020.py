318053024cec651434aa5f960a3c63a652685b6d
Eric Brandwein <brandweineric@gmail.com>
2020-04-14 20:12:24 -0300

    Migration 0028 usa RunPython en vez de una operation custom
    Porque no me estaba andando en heroku.

--- secomplican/enunciados/migrations/0028_materias_optativas_cuatri1_2020.py
+++ secomplican/enunciados/migrations/0028_materias_optativas_cuatri1_2020.py
@@ -1,6 +1,5 @@
 from django.db import migrations
 from django.utils.text import slugify
-from enunciados.migrations.operations.agregar_materias_optativas_de_computacion_operation import AgregarMateriasOptativasDeComputacionOperation
 
 
 MATERIAS = [
@@ -10,6 +9,33 @@ MATERIAS = [
     'Toma de Decisiones'
 ]
 
+
+def agregar_materias(apps, schema_editor):
+    """Agrega las materias optativas a la base de datos."""
+    Carrera = apps.get_model('enunciados', 'Carrera')
+    MateriaCarrera = apps.get_model('enunciados', 'MateriaCarrera')
+    Materia = apps.get_model('enunciados', 'Materia')
+
+    computacion = Carrera.objects.get(slug='computacion')
+    for nombre_materia in MATERIAS:
+        materia = Materia.objects.create()
+        slug = slugify(nombre_materia)
+        MateriaCarrera.objects.create(
+            carrera=computacion, materia=materia, nombre=nombre_materia,
+            slug=slug, optativa=True)
+
+
+def sacar_materias(apps, schema_editor):
+    Carrera = apps.get_model('enunciados', 'Carrera')
+    MateriaCarrera = apps.get_model('enunciados', 'MateriaCarrera')
+    Materia = apps.get_model('enunciados', 'Materia')
+
+    computacion = Carrera.objects.get(slug='computacion')
+    for nombre_materia in MATERIAS:
+        slug = slugify(nombre_materia)
+        MateriaCarrera.objects.get(carrera=computacion, slug=slug).materia.delete()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -17,5 +43,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        AgregarMateriasOptativasDeComputacionOperation(MATERIAS),
+        migrations.RunPython(agregar_materias, sacar_materias),
     ]
