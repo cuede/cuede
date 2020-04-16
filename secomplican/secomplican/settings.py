b57dcc1fbeb163427b2585e8221d0360de2243a9
Eric Brandwein <brandweineric@gmail.com>
2020-04-16 19:38:31 -0300

    Removes PROJECT_ROOT from settings.py

--- secomplican/secomplican/settings.py
+++ secomplican/secomplican/settings.py
@@ -17,7 +17,6 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # .../qed/secomplican
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
