a942ba470126cb6ab7ca6d1a3647cb7c7f68225f
Eric Brandwein <brandweineric@gmail.com>
2020-04-16 18:40:20 -0300

    Sets STATICFILES_STORAGE

--- secomplican/secomplican/settings.py
+++ secomplican/secomplican/settings.py
@@ -139,6 +139,10 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
