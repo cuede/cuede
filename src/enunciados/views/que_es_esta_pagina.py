d1989dd09335961a7dc8d5d262a44724d24d7bbb
Eric Brandwein <brandweineric@gmail.com>
2020-04-20 15:13:41 -0300

    Muestra botón de Materias en vista que_es_esta_pagina

--- src/enunciados/views/que_es_esta_pagina.py
+++ src/enunciados/views/que_es_esta_pagina.py
@@ -1,5 +1,7 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
+from enunciados.models import Carrera
 
 
 def que_es_esta_pagina(request):
-    return render(request, 'que_es_esta_pagina.html')
+    computacion = Carrera.objects.get(slug='computacion')
+    return render(request, 'que_es_esta_pagina.html', {'carrera': computacion})
