4bdd0c41d402a101d5ab4ac3bf835e9db195000e
Eric Brandwein <brandweineric@gmail.com>
2020-04-26 18:23:46 -0300

    Deja que puedas correr run_tests.sh sin pasarle argumentos

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo docker exec -it cuede_webserver_1 bash -c "cd src; python manage.py test '$1'"
+sudo docker exec -it cuede_webserver_1 bash -c "cd src; python manage.py test $1"
