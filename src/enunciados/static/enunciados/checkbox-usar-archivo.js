f48aceed32f64cf987f9424a49103c4bc31f0d88
Eric Brandwein <brandweineric@gmail.com>
2020-05-01 18:40:20 -0300

    Vuelve a fijarse si hay que mostrar editor después de colapsarlo
    Para que no pase que si apretás muy rápido no se te muestre más.

--- src/enunciados/static/enunciados/checkbox-usar-archivo.js
+++ src/enunciados/static/enunciados/checkbox-usar-archivo.js
@@ -1,12 +1,20 @@
 const checkboxId = "editor-disable-checkbox";
 
-function toggleHideEditor() {
-    $('#editor-container').collapse("toggle")
+function toggleHideEditor(checkbox, editor) {
+    if (checkbox.checked) {
+        editor.collapse("hide");
+    } else {
+        editor.collapse("show");
+    }
 }
 
 function toggleDisabledEditorOnCheckboxClick() {
     const checkbox = document.getElementById(checkboxId);
-    checkbox.onclick = toggleHideEditor;
+    const editorContainer = $('#editor-container');
+    const updateEditorVisibility = () => {toggleHideEditor(checkbox, editorContainer)};
+    checkbox.onclick = updateEditorVisibility;
+    editorContainer.on('hidden.bs.collapse', updateEditorVisibility);
+    editorContainer.on('shown.bs.collapse', updateEditorVisibility);
 }
 
 document.addEventListener("DOMContentLoaded", toggleDisabledEditorOnCheckboxClick, false);
