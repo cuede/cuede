7ebe3a1f9bfe07e1b35f4cee63f423294e074b60
Eric Brandwein <brandweineric@gmail.com>
2020-05-01 14:23:53 -0300

    Manda texto vacío cuando checkbox de enunciado está checkeado

--- src/enunciados/static/enunciados/checkbox-usar-archivo.js
+++ src/enunciados/static/enunciados/checkbox-usar-archivo.js
@@ -1,10 +1,19 @@
+const checkboxId = "editor-disable-checkbox";
+
 function toggleHideEditor() {
     $('#editor-container').collapse("toggle")
 }
 
 function toggleDisabledEditorOnCheckboxClick() {
-    const checkbox = document.getElementById("editor-disable-checkbox");
+    const checkbox = document.getElementById(checkboxId);
     checkbox.onclick = toggleHideEditor;
 }
 
 document.addEventListener("DOMContentLoaded", toggleDisabledEditorOnCheckboxClick, false);
+
+form.onsubmit = function () {
+    const checkbox = document.getElementById(checkboxId);
+    if (!checkbox.checked) {
+        textElement.innerHTML = quill.getText();
+    }
+}
