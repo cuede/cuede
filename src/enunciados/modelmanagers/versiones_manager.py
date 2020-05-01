44048a9277db3fcb385ddf5771051ebbd1f973ee
Eric Brandwein <brandweineric@gmail.com>
2020-05-01 17:06:21 -0300

    Se puede editar enunciado vacío

--- src/enunciados/modelmanagers/versiones_manager.py
+++ src/enunciados/modelmanagers/versiones_manager.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class VersionesManager(models.Manager):
     def ultima(self):
+        from enunciados.models import VersionTexto
         queryset = self.get_queryset()
         if queryset:
             return queryset.all()[0]
         else:
-            return ''
+            return VersionTexto(texto='')
