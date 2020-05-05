4afab45bdaee08b62379e5d03e34bb920065ba13
Eric Brandwein <brandweineric@gmail.com>
2020-05-05 01:50:02 -0300

    Limpia un poco editor.js

--- src/enunciados/static/enunciados/editor.js
+++ src/enunciados/static/enunciados/editor.js
@@ -1,4 +1,4 @@
-var quill = new Quill('#editor', {
+const quill = new Quill('#editor', {
     theme: 'snow',
     formats: ['code', 'formula'],
     modules: {
@@ -6,14 +6,9 @@ var quill = new Quill('#editor', {
     },
 });
 
-function surroundSelectionBy(before, after) {
-    var selection = quill.getSelection();
-    if (selection) {
-        quill.insertText(selection.index + selection.length, after)
-        quill.insertText(selection.index, before);
-        quill.setSelection(selection.index + before.length, selection.length)
-    }
-}
+const toolbar = quill.getModule('toolbar');
+toolbar.addHandler('code', toolbarCodeHandler);
+toolbar.addHandler('formula', toolbarFormulaHandler);
 
 function toolbarCodeHandler() {
     surroundSelectionBy('```\n', '\n```\n');
@@ -23,17 +18,21 @@ function toolbarFormulaHandler() {
     surroundSelectionBy('\\[\n', '\n\\]\n');
 }
 
-var toolbar = quill.getModule('toolbar');
-toolbar.addHandler('code', toolbarCodeHandler);
-toolbar.addHandler('formula', toolbarFormulaHandler);
+function surroundSelectionBy(before, after) {
+    const selection = quill.getSelection();
+    if (selection) {
+        quill.insertText(selection.index + selection.length, after)
+        quill.insertText(selection.index, before);
+        quill.setSelection(selection.index + before.length, selection.length)
+    }
+}
 
-var textElement = document.getElementById('hidden_textarea');
-var form = document.getElementById('form');
-
+const form = document.getElementById('form');
 form.onsubmit = putEditorTextInHiddenTextArea;
 
 function putEditorTextInHiddenTextArea() {
     const text = quill.getText();
+    const textElement = document.getElementById('hidden_textarea');
     textElement.innerHTML = text;
     if (text.trim() === "") {
         const emptyTextError = document.getElementById("empty-text-error");
